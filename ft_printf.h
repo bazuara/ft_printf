d1646cbcbc95b17126fa2ca87c79e4a635137e9e
Bruno Azuara <bruno.azuara@gmail.com>
2019-12-06 15:46:58 +0100

    Subida automática

--- ft_printf.h
+++ ft_printf.h
@@ -1,32 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bazuara <bazuara@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/12/06 13:13:03 by bazuara           #+#    #+#             */
-/*   Updated: 2019/12/06 15:46:56 by bazuara          ###   ########.fr       */
+/*   Created: 2019/12/06 15:42:46 by bazuara           #+#    #+#             */
+/*   Updated: 2019/12/06 15:46:55 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#ifdef FT_PRINT_H
+# define FT_PRINT_H
 
-int	ft_printf(const char *str /*, ...*/)
-{
-	while (str && *str != '\0')
-	{
-		ft_putchar_fd(*str, 1);
-		str++;
-		if(*str == '%')
-		{
-			str++;
-			if(*str == 'd')
-			{
-				ft_putchar_fd('X', 1);
-				str++;
-			}
-		}
-	}
-	return (0);
-}
+# include <cstdarg.h>
+# include "ft_lib/libft.h"
+
+#endif
