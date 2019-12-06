94fa9abdef00b866b5c651c2616b4a40558d50da
Bruno Azuara <bruno.azuara@gmail.com>
2019-12-06 13:39:18 +0100

    Subida automática

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,15 @@
 /*   By: bazuara <bazuara@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/06 13:13:03 by bazuara           #+#    #+#             */
-/*   Updated: 2019/12/06 13:34:06 by bazuara          ###   ########.fr       */
+/*   Updated: 2019/12/06 13:39:09 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "ft_lib/libft.h"
 
-int	ft_printf(const char * /*, ...*/)
+int	ft_printf(const char *str /*, ...*/)
 {
-	
+	ft_putstr_fd("prueba ", 7);
+	ft_putstr_fd((char *)str, ft_strlen(str));
+	return (0);
 }
