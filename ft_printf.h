9e73cadff9889aadcd2aca176ac0c57ad36ddf5c
Bruno Azuara <bruno.azuara@gmail.com>
2019-12-08 11:35:00 +0100

    Subida automática

---
+++ ft_printf.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bazuara <bazuara@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/06 15:42:46 by bazuara           #+#    #+#             */
+/*   Updated: 2019/12/07 02:34:38 by bazuara          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+
+typedef	struct	s_type
+{
+	int is_char;
+	int is_str;
+	int is_ptr;
+	int	is_dec;
+	int	is_int;
+	int	is_uns;
+	int	is_minhex;
+	int is_mayhex;
+}				t_type;
+
+typedef struct	s_flags
+{
+	char	*flag_str;
+}				t_flags;
+#endif
