b854b5cee9bb0f6c0bf80304fb8d0db0e871b4c2
Bruno Azuara <bruno.azuara@gmail.com>
2019-12-10 13:55:48 +0100

    Subida automática

--- ft_printint.c
+++ ft_printint.c
@@ -6,7 +6,7 @@
 /*   By: bazuara <bazuara@student.42madrid.>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/09 15:37:39 by bazuara           #+#    #+#             */
-/*   Updated: 2019/12/10 12:33:06 by bazuara          ###   ########.fr       */
+/*   Updated: 2019/12/10 13:54:43 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ const char	*ft_printint(const char *str,va_list args, int **count, t_flags **fla
 	int i;
 
 	i = va_arg(args, int);
-	//gestionar numeros que son espacios en blanco antes del int
-	
 	if ((*flags)->is_plus == 1 && i >= 0)
 	{
 		ft_putchar_fd('+', 1);
