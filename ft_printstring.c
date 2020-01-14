796e644fdfa871fb756fac542bc0eaaef3f37235
Bruno Azuara <bruno.azuara@gmail.com>
2020-01-14 15:08:10 +0100

    Subida automática

--- ft_printstring.c
+++ ft_printstring.c
@@ -6,7 +6,7 @@
 /*   By: bazuara <bazuara@student.42madrid.>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/14 11:05:30 by bazuara           #+#    #+#             */
-/*   Updated: 2020/01/14 12:11:19 by bazuara          ###   ########.fr       */
+/*   Updated: 2020/01/14 14:52:31 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,19 @@ const char	*ft_printstring(const char *str, va_list args, int **count,
 			t++;
 		}*/
 		(*(*count)) = ft_prespace((*flags)->width, ft_strlen(c), *(*(count)),
-				(((*flags)->is_zero == 1) ? '0' : ' '));
+		(((*flags)->is_zero == 1) ? '0' : ' '));
 	ft_putstr_fd(c, 1);
 	(*(*count)) += ft_strlen(c);
 	str++;
 	if ((*flags)->is_minus == 1 && (*flags)->width > 0)
-		while (t < ((*flags)->width - ft_strlen(c)))
+		/*while (t < ((*flags)->width - ft_strlen(c)))
 		{
 			ft_putchar_fd((((*flags)->is_zero == 1) ? '0' : ' '), 1);
 			(*(*count))++;
 			t++;
-		}
+		}*/
+		(*(*count)) = ft_prespace((*flags)->width, ft_strlen(c), *(*(count)),
+		(((*flags)->is_zero == 1) ? '0' : ' '));
+
 	return (str);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(void) {
 //           printf("sys: ");
 //	       printf("test, %.i", 5); printf("\n");
 		   //ft_printf("ft_: ");
-		   res = ft_printf("%32s", "abc"); 
+		   res = ft_printf("%-32s Hola_%s", "abc", "def"); 
 //		   printf("\nres: %i", res);
   return 0;
 }
