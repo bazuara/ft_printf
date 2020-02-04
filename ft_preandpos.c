38fd05b1349fb2997f88b085fc4f38ea8a7ce5ec
Bruno Azuara <bruno.azuara@gmail.com>
2020-02-04 17:13:33 +0100

    pre fixed in strings

--- ft_preandpos.c
+++ ft_preandpos.c
@@ -6,25 +6,26 @@
 /*   By: bazuara <bazuara@student.42madrid.>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/14 11:49:26 by bazuara           #+#    #+#             */
-/*   Updated: 2020/02/04 15:59:29 by bazuara          ###   ########.fr       */
+/*   Updated: 2020/02/04 17:12:13 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_prespace(size_t width, size_t length, int count, char filler)
+int	ft_prespace(t_flags **flags, size_t length, int count, char filler)
 {
 	int	t;
 	int	dif;
 
 	t = 0;
-	//printf("debug preandpos length: %zu \n", length);
-	//printf("debug preandpos width: %zu \n", width);
-	dif = (int)width - (int)length;
-	while ((t < (dif)) && (dif > 0))
+	//printf("debug pre length: %zu \n", length);
+	//printf("debug pre width: %zu \n", (*flags)->width);
+	dif = (int)(*flags)->width - (int)length;
+	//printf("pre t: %i, width: %zu, length: %zu, count: %i dif: %i\n", t, (*flags)->width, length, count, dif);
+	while (((t < (dif)) && (dif > 0)) || (t <  ((int)(*flags)->width - (int)(*flags)->decimals)))
 	{
 		ft_putchar_fd(filler, 1);
-		//printf("pre t: %zu, width: %zu, length: %zu, count: %i dif: %i\n", t, width, length, count, dif);
+		//printf("pre t: %i, width: %zu, length: %zu, count: %i dif: %i\n", t, (*flags)->width, length, count, dif);
 		count++;
 		t++;
 	}
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: bazuara <bazuara@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/06 15:42:46 by bazuara           #+#    #+#             */
-/*   Updated: 2020/02/04 15:43:08 by bazuara          ###   ########.fr       */
+/*   Updated: 2020/02/04 17:04:41 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,8 @@ const char		*ft_checkflags(const char *str, int *count, t_flags *flags);
 const char		*ft_printint(const char *str,va_list args, int **count,
 				t_flags **flags);
 size_t			ft_intlen(size_t n);
-int				ft_prespace(size_t width, size_t length, int count, char filler);
+int				ft_prespace(t_flags **flags, size_t length, int count,
+				char filler);
 int				ft_posspace(size_t width, size_t length, int count, char filler);
 const char		*ft_printstring(const char *str, va_list args, int **count,
 				t_flags **flags);
