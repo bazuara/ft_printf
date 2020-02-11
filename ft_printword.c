482e1944e8fb625f3a8844d4e122fb20c6868ab6
Bruno Azuara <bruno.azuara@gmail.com>
2020-02-11 13:45:30 +0100

    pendiente segfault y nulls con width

--- ft_printword.c
+++ ft_printword.c
@@ -6,7 +6,7 @@
 /*   By: bazuara <bazuara@student.42madrid.>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/14 15:09:53 by bazuara           #+#    #+#             */
-/*   Updated: 2020/02/11 12:34:30 by bazuara          ###   ########.fr       */
+/*   Updated: 2020/02/11 13:45:25 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,19 @@ int	ft_printword(char *word, t_flags **flags, int count)
 	i = 0;
 	c = count;
 	ft_strlcpy(nullstring, "(null)", 7);
-	if ((!word || *word == 0 )&& (*flags)->is_num == 1)
+	if ((!word || *word == '\0') && ((*flags)->width > 0 || (*flags)->precission > 0 ))
 	{
 		if ((*flags)->is_point == 1 && (*flags)->width > 0)
+		{
 			while (i < 6 && i < (*flags)->width)
 			{
 				ft_putchar_fd(' ',1);
 				i++;
 			}
+			return (c);
+		}
 		else
-			while (nullstring[i] != '\0' && (((*flags)->precission) == 0 || (i < ((*flags)->precission))))
+			while (nullstring[i] != '\0' && (((*flags)->precission) == 0 || (i < ((*flags)->precission))) /*&& (*flags)->width != 0*/)
 			{
 				ft_putchar_fd(nullstring[i], 1);
 				i++;
@@ -40,7 +43,7 @@ int	ft_printword(char *word, t_flags **flags, int count)
 	}
 	if ((*flags)->precission == 0 && (*flags)->is_point == 1)
 		return (c);
-	//ft_putstr_fd("peta aqui",1);
+	i = 0;
 	if ((*flags)->precission == 0)
 		while ((word[i] != '\0'))
 		{
