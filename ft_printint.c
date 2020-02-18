90c52776f251191c4011feb847e232ebf5594c9d
Bruno Azuara <bruno.azuara@gmail.com>
2020-02-18 15:00:40 +0100

    pre int

--- ft_printint.c
+++ ft_printint.c
@@ -6,7 +6,7 @@
 /*   By: bazuara <bazuara@student.42madrid.>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/09 15:37:39 by bazuara           #+#    #+#             */
-/*   Updated: 2020/02/18 12:53:20 by bazuara          ###   ########.fr       */
+/*   Updated: 2020/02/18 14:58:21 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,21 +39,27 @@ const char	*ft_printint(const char *str, va_list args, int **count,
 			free (filler);
 		}
 	}
-	if (i < 0)//TODO fix
+	if ((*flags)->width > ft_strlen(ft_itoa(abs)) && i < 0 && (*flags)->is_zero == 0)
 		num = ft_strjoin("-", num);
 	if ((*flags)->width > ft_intlen(abs))
 		if ((*flags)->width > ft_strlen(num))
 		{
 			filler = ft_calloc((*flags)->width - ft_strlen(num) + 1, sizeof(char));
-			ft_memset(filler, ((*flags)->is_zero == 1 ? 'X' : ' '), (*flags)->width - ft_strlen(num));
+			ft_memset(filler, ((*flags)->is_zero == 1 ? '0' : ' '), (*flags)->width - ft_strlen(num));
 			if ((*flags)->is_minus == 1)
 				num = ft_strjoin(num, filler);
 			else
 				num = ft_strjoin(filler, num);
-			if (i < 0 && (*flags)->is_zero == 1)
-				ft_memset(num, '-', 1);
 			free (filler);
 		}
+	if (i < 0)
+	{
+		if ((*flags)->width == 0 || (*flags)->width <= ft_strlen(ft_itoa(abs)))
+			num = ft_strjoin("-", num);
+		if ((*flags)->width > ft_strlen(ft_itoa(abs)) && (*flags)->is_zero == 1)
+			num[0] = '-';
+
+	}
 	ft_putstr_fd(num, 1);
 	(*(*count)) += ft_strlen(num);
 	str++;
--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(void) {
 	int res;
 //           printf("sys: ");
 //	       printf("test, %.i", 5); printf("\n");
-			res = ft_printf("%.3i", 13862);
+			res = ft_printf("%08.3i", 8375);
 //		   printf("\nres: %i", res);
   return 0;
 }
