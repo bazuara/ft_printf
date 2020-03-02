eecbf4fdd24013918cd607e3c53bba14b459b83e
Bruno Azuara <bruno.azuara@gmail.com>
2020-03-02 18:57:05 +0100

    cleanup hex and preparing for flags

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(void) {
 	int res;
 //           printf("sys: ");
 //	       printf("test, %.i", 5); printf("\n");
-			res = ft_printf("this %x number", 7562);
+			res = ft_printf("this %x number", 0);
 			ft_putstr_fd("\n counter: ", 1);
 			ft_putnbr_fd(res, 1);
   return 0;
