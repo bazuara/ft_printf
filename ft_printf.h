cd68b5ed9c3d7e7c073635ad1b26c91db23ce2ce
Bruno Azuara <bruno.azuara@gmail.com>
2020-04-14 19:37:35 +0200

    Fixed header according to Norme

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: bazuara <bazuara@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/06 15:42:46 by bazuara           #+#    #+#             */
-/*   Updated: 2020/04/14 17:56:02 by bazuara          ###   ########.fr       */
+/*   Updated: 2020/04/14 19:36:50 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,40 +26,40 @@ typedef	struct	s_flags
 	int			is_space;
 	int			is_zero;
 	int			is_hash;
-    int         is_num;
+	int			is_num;
 	int			is_point;
 	int			is_asterisk;
 	int			has_precission;
 	size_t		precission;
 	int			has_width;
-    size_t      width;
+	size_t		width;
 }				t_flags;
 
 const char		*ft_printsymbol(const char *str, t_flags **flags, int **count);
-const char		*ft_checkflags_sym(const char *str, va_list args ,t_flags *flags);
+const char		*ft_checkflags_sym(const char *str, va_list args,
+		t_flags *flags);
 const char		*ft_checkflags_num(const char *str, t_flags *flags);
 char			*ft_uitoa(unsigned long long int n);
 char			*ft_strtoupper(char *str);
-const char		*ft_printint(const char *str,va_list args, int **count,
+const char		*ft_printint(const char *str, va_list args, int **count,
 				t_flags **flags);
 int				ft_prespace(t_flags **flags, size_t length, int count,
 				char filler);
-int				ft_posspace(size_t width, size_t length, int count, char filler);
+int				ft_posspace(size_t width, size_t length, int count,
+		char filler);
 const char		*ft_printstring(const char *str, va_list args, int **count,
 				t_flags **flags);
 const char		*ft_printchar(const char *str, va_list args, int **count,
 				t_flags **flags);
 int				ft_printword(char *str, t_flags **flags, int count);
 char			*ft_long_itoa(long int nbr);
-//char			*ft_strrev(char *str);
-//char			ft_numtohex(unsigned int n);
 char			*ft_uitohex(unsigned int q);
 char			*ft_ultohex(unsigned long q);
-const char		*ft_printuint(const char *str,va_list args, int **count,
+const char		*ft_printuint(const char *str, va_list args, int **count,
 				t_flags **flags);
-const char		*ft_printhex(const char *str,va_list args, int **count,
+const char		*ft_printhex(const char *str, va_list args, int **count,
 				t_flags **flags);
-const char		*ft_printpointer(const char *str,va_list args, int **count,
+const char		*ft_printpointer(const char *str, va_list args, int **count,
 				t_flags **flags);
 int				ft_isflag(char *str);
 int				max_int(int a, int b);
